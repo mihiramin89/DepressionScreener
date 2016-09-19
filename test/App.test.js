b4d155c5e80df4a75e4e3c861ca8ba7bd7181d2a
Mihir Amin <mihiramin89@gmail.com>
2016-09-19 15:37:14 -0400

    added a testcase for rendering and cleaned up the code a bit

--- test/App.test.js
+++ test/App.test.js
@@ -1,22 +1,32 @@
 jest.unmock('../App');
 
-  //import React from 'react';
-  //import ReactDOM from 'react-dom';
-  //import TestUtils from 'react-addons-test-utils';
+  import React from 'react';
+  import ReactDOM from 'react-dom';
+  import TestUtils from 'react-addons-test-utils';
   import App from '../App';
 
+//before each test, reset the modules so we start at a clean instance.
+beforeEach(function() {
+	jest.resetModules();
+});
 
 describe("App Test", function() {
 
-	var React = require('react');
-	var ReactDOM = require('react-dom');
-	var TestUtils = require('react-addons-test-utils');
-
-	it("class is defined",  function() {
+	it("class is defined", function() {
 
 		var instance = new App();
 
 		expect(instance.constructor.name).toEqual("App");
 		
 	});
+
+	it("is rendered",function() {
+		//var instance = new App();
+
+		var instance = TestUtils.renderIntoDocument( <App /> );
+    	expect(TestUtils.isCompositeComponent(instance)).toBeTruthy();
+	});
 });
+
+
+
